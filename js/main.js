6732e44c1ec6f1c4b504e1d3fdd5c80437ce3f08
Lucas Pedreiro <lucas.pedreiro@sigcertificadora.com.br>
2019-08-29 11:58:10 -0300

    Ajuste no html e css do formulário e texto da seção 'proposta'.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
     'use strict'
-    /* Jquery Mask Plugin */
-    $('.phone_with_ddd').mask('(00) 00000-0000');
+    /* Jquery Mask Plugin
+    Plugin que mascara os dados inseridos pelo usuário no formulário de contato.*/
+    $('#contato-form-fone').mask('(00) 00000-0000');
 
-    
+
 });
 
