e74d8001e988a2c08950a83e17a0e56149a7d6d1
Lucas Pedreiro <lucas.pedreiro@sigcertificadora.com.br>
2019-09-05 08:31:01 -0300

    Plugin autocomplete alterado.

--- js/main.js
+++ js/main.js
@@ -8,16 +8,15 @@ $(document).ready(function () {
     Plugin que mascara os dados inseridos pelo usuário no formulário de contato.*/
     $('.contato-form-fone').mask('(00) 00000-0000');
 
-    /* Awesomplete Plugin */
-    new Awesomplete('input[type="email"]', {
-        list: ["ig.com.br", "globo.com", "bol.com.br", "gmail.com", "googlemail.com", "google.com", "hotmail.com", "outlook.com", "msn.com", "live.com", "yahoo.com", "yahoo.com.br", "terra.com.br"],
-        data: function (text, input) {
-            return input.slice(0, input.indexOf("@")) + "@" + text;
-        },
-        filter: Awesomplete.FILTER_STARTSWITH
-    });
+    /* Jquery Plugin AutoComplete */
+    $('.contato-form-email').autoEmail(
+        [
+            "ig.com.br", "globo.com", "bol.com.br", "googlemail.com", "google.com", "hotmail.com", "outlook.com", "msn.com", "live.com", "yahoo.com", "yahoo.com.br", "terra.com.br"
+        ],
+        'gmail.com'
+    )
 
-    // smooth scrolling
+    /* Smooth Scroll */
     var scrollLink = $('.scroll');
 
     scrollLink.click(function (e) {
